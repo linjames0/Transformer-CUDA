3696523e70d8b13d3bf5543b9acf0fd162832927
James <78285353+linjames0@users.noreply.github.com>
2023-09-23 21:33:00 -0400

    Update transpose.cu

--- transpose.cu
+++ transpose.cu
@@ -5,6 +5,7 @@ __global__ void transpose(float *d_A, float *d_T, int M, int N) {
 	int row = blockIdx.y * blockDim.y + threadIdx.y;
 	int col = blockIdx.x * blockDim.x + threadIdx.x;
 
+	// swap elements via transpose
 	if (row < M && col < N) {
 		d_T[col * M + row] = d_A[row * N + col];
 	}
